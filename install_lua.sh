d477678916323421bde3a240fc39e6755a7c3490
huangxiang <rover0321@qq.com>
2019-04-15 14:22:01 +0800

    Description: install lua shell

---
+++ install_lua.sh
@@ -0,0 +1,9 @@
+line
+echo "Install lua interceptor."
+sudo apt-get install  lua5.3
+sudo ln -s /usr/bin/lua5.3 /usr/bin/lua
+line
+echo "Install lua dev environment."
+sudo apt-get install lua5.3-dev
+line
+echo "Installation finish."
